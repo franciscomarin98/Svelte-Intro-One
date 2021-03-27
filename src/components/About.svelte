4dc556369a050bec68b0ff4da2a4cef88c873e2b
francisco-1998 <francmarinc@gmail.com>
2021-03-27 11:22:48 -0500

    Reactividad creando un contador

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,9 +1,16 @@
 <script>
     let someText = "Frontend Developer at Home";
+    let count = 0;
+    const handleIncrement = () =>{
+        count++;
+    }
 </script>
 
 <div class="About">
     <p>{someText}</p>
+    <button on:click={handleIncrement}>
+        Click {count === 0 ? '' : count}
+    </button>
 </div>
 
 <style>
