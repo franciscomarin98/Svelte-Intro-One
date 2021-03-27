addba9426b5a31369fce802b9b404de5aeeb6991
francisco-1998 <francmarinc@gmail.com>
2021-03-27 11:47:51 -0500

    Entendiendo los props

---
+++ src/components/Person.svelte
@@ -0,0 +1,11 @@
+<script>
+    // your script goes here
+    export let name;
+    export let lastName;
+    export let age;
+</script>
+
+<!-- markup (zero or more items) goes here -->
+<div class="Person">
+    <p>{name} {lastName} {age}</p>
+</div>
---
+++ src/components/Text.svelte
@@ -0,0 +1,15 @@
+<script>
+    // your script goes here
+    export let anotherText = 'Default value of my prop';
+</script>
+
+<!-- Si no vamos a  usar estilos o script, no es necesario que definamos la sección -->
+
+<!-- <style>
+    /* your styles go here */
+</style> -->
+
+<!-- markup (zero or more items) goes here -->
+<div class="Text">
+    <p>{anotherText}</p>
+</div>
