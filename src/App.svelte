bfc0bb8cd640d63ab9a47899f7b2628717b8c313
francisco-1998 <francmarinc@gmail.com>
2021-03-26 21:01:39 -0500

    Entiendo el flujo de los estilos en svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,35 @@
 <script>
 	export let name;
 	export let lastName;
+	let svelteLogo = 'https://arepa.s3.amazonaws.com/svelte-logo.png';
 </script>
 
 <main>
 	<h1>Hello {name} {lastName}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>Frontend Developer</p>
+	<img src={svelteLogo} alt="Svelte Logo">
+
 </main>
 
 <style>
+	/* Definir estilos globales */
+	:global(body){
+		background-color: #f2eee2;
+		color: #0084f6;
+	}
+
+	/* Definir variables */
+	:global(:root){
+		--theme-color: purple;
+	}
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+	}
+	main img {
+		width:30%;
 	}
 
 	h1 {
@@ -21,6 +37,11 @@
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+
+	p{
+		color: var(--theme-color);
+		font-size: 2rem;
 	}
 
 	@media (min-width: 640px) {
